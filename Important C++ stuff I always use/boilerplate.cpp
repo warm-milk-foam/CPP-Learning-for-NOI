98af2ba82410aa86bf0eeaafed3d2907d4b29bee
chocolate_milk <129393705+warm-milk-foam@users.noreply.github.com>
2026-02-06 20:38:26 +0800

    Create boilerplate.cpp

---
+++ Important C++ stuff I always use/boilerplate.cpp
@@ -0,0 +1,14 @@
+// The boiler starter code I should always use...
+// on mac, use g++ -std=gnu++17 boilerplate.cpp -O2 -o main && ./main
+#include <bits/stdcc+.h>
+using namespace std;
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "It works!!" << "\n";
+    return 0;
+}
