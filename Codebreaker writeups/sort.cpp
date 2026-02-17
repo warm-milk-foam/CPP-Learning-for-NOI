9977624b0badcdd03d316333a9629c62b17003e3
Ingo <ingocodingbecauseyes@gmail.com>
2026-02-17 14:09:33 +0800

    sort.cpp

---
+++ Codebreaker writeups/sort.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<string> numbers(N);
+    for (int i = 0; i < N; i++) cin >> numbers[i];
+
+    sort(numbers.begin(), numbers.end(), [](const string &a, const string &b) {
+        if (a.size() != b.size()) return a.size() < b.size();
+        return a < b;
+    });
+
+    for (int i = 0; i < N; i++) {
+        if (i) cout << " ";
+        cout << numbers[i];
+    }
+    cout << '\n';
+}
+// chatgpt solution, full solves...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int alt() {
+	long long N;
+	cin >> N;
+	vector<long long> numbers(N);
+	for (long long i = 0; i < N; i++) cin >> numbers[i];
+	
+	sort(numbers.begin(), numbers.end());
+	for (long long i = 0; i < N; i++) cout << numbers[i] << " ";
+	cout << '\n';
+}	
+// original solution...
+
+// this challenge is trickery because they want you to know that long long isnt enough
